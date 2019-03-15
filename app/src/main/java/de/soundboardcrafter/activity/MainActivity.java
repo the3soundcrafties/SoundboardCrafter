da801ebe6915ecd5781db6f69489a40577aec3ec
jfelkel <julia.schechter.it@googlemail.com>
2019-03-15 20:27:22 +0100

    init project

---
+++ app/src/main/java/de/soundboardcrafter/activity/MainActivity.java
@@ -0,0 +1,15 @@
+package de.soundboardcrafter.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+import de.soundboardcrafter.R;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
