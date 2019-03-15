a786c50b6bdde2c362846348f88b7f94ee7a6040
jfelkel <julia.schechter.it@googlemail.com>
2019-03-15 22:57:33 +0100

    fragment hinzugefügt mit button

---
+++ app/src/main/java/de/soundboardcrafter/activity/MainFragment.java
@@ -0,0 +1,33 @@
+package de.soundboardcrafter.activity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import de.soundboardcrafter.R;
+
+public class MainFragment extends Fragment {
+
+    public MainFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
+        return rootView;
+    }
+
+}
