8b84c9f42deee088ac7c14663f5e04d872695184
jfelkel <julia.schechter.it@googlemail.com>
2019-03-30 18:55:36 +0100

    ViewPager to Slide in the activity

--- app/src/main/java/de/soundboardcrafter/activity/soundboard/play/MainActivity.java
+++ app/src/main/java/de/soundboardcrafter/activity/soundboard/play/MainActivity.java
@@ -1,14 +1,61 @@
 package de.soundboardcrafter.activity.soundboard.play;
 
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import de.soundboardcrafter.activity.common.SingleFragmentActivity;
+import androidx.fragment.app.FragmentActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentStatePagerAdapter;
+import androidx.viewpager.widget.PagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+import de.soundboardcrafter.R;
 
 /**
  * The main activity, showing the soundboards.
  */
-public class MainActivity extends SingleFragmentActivity {
+public class MainActivity extends FragmentActivity {
+    private ViewPager pager;
+    private PagerAdapter pagerAdapter;
+
+
     @Override
-    protected Fragment createFragment() {
-        return new SoundboardFragment();
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fragment);
+
+        // ViewPager and its adapters use support library
+        // fragments, so use getSupportFragmentManager.
+        pagerAdapter =
+                new ScreenSlidePagerAdapter(
+                        getSupportFragmentManager());
+        pager = (ViewPager) findViewById(R.id.viewPager);
+        pager.setAdapter(pagerAdapter);
+    }
+
+
+    class ScreenSlidePagerAdapter extends FragmentStatePagerAdapter {
+
+        ScreenSlidePagerAdapter(@NonNull FragmentManager fm) {
+            super(fm);
+        }
+
+
+        @Override
+        public Fragment getItem(int position) {
+            return new SoundboardFragment();
+        }
+
+        @Override
+        public int getCount() {
+            return 1;
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return "OBJECT " + (position + 1);
+        }
+
+
     }
 }
